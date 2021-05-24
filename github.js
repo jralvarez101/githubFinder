7adee383b17cfde6dfc9438d43d8e90127fe00f9
martin <alvarez.martinjr@gmail.com>
2021-05-24 10:07:51 -0700

    added repo display UI

--- github.js
+++ github.js
@@ -2,8 +2,12 @@ class GitHub {
 
     // As we are using a class with a constructor we will be using the client id and client secret
     constructor(){
-        this.client_id = '3bd22df8ebf9c033d6fa',
-        this.client_secret = '6ed66c1baaca823f440cd1c10307592d2fc3ca6e'
+        this.client_id = '3bd22df8ebf9c033d6fa';
+        this.client_secret = '6ed66c1baaca823f440cd1c10307592d2fc3ca6e';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+
+
 
     }
 
@@ -11,10 +15,16 @@ class GitHub {
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+        const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}
+        &sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
         const profile = await profileResponse.json();
+        const repos = await reposResponse.json();
+        
 
         return {
-            profile
+            profile,
+            repos
         }
     }
 }
--- ui.js
+++ ui.js
@@ -33,6 +33,34 @@ class UI{
         `;
     }
 
+    // Show repos
+    showRepos(repos){
+        
+        // TIP: use output variable when working with arrays (repos is an array)
+        let output= '';
+
+        repos.forEach(repo => {
+            output+=`
+            <div class="card card-body mb-2">
+                <div class="row">
+                    <div class= "col-md-6">
+                        <a href="${repo.html_url} target="blank" >${repo.name}</a>
+                    </div>
+                    <div class= "col-md-6">
+                    <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+                    <span class="badge badge-secondary ">Watchers ${repo.watchers_count}</span>
+                    <span class="badge badge-success ">Forks: ${repo.forks_count}</span>
+                    </div>
+                </div>
+            </div>
+            `;
+        })
+        
+        // Output repositories
+        document.getElementById('repos').innerHTML = output;
+
+    }
+
     // Show alert message
     showAlert(message, className){
         // Clear any remaining alerts
